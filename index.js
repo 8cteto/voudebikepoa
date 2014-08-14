24485e6d097c74bbf8da8ca2244c6e3152ce4c88
Eduardo Bohrer <nbluisrs@gmail.com>
2014-08-14 20:31:49 -0300

    running a database query to teste connection

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 var express = require('express'),
       config  = require('./config'),
+      pg = require('pg'); 
       app  = express();
 
 app.get('/', function(req, res){
-  res.send('Vour de Bike!');
+
+    db = new pg.Client(config.DATABASE_URL);
+
+    db.connect(function(err) {
+        if (err) {
+            res.send('could not connect to postgres');
+            return;
+        }
+
+        db.query('SELECT \'Vour de Bike!\' as text', function(err, result) {
+            db.end();
+            if (err) {
+                res.send('error running query', err);
+                return;
+            }
+
+            res.send(result.rows[0]['text']);
+            db.end();
+      });
+    });
 });
 
 app.listen(config.WEBAPP_PORT);
