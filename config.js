2418b8e65804bcaff3621a63c69e4836cc1bef74
Eduardo Bohrer <nbluisrs@gmail.com>
2014-08-14 20:18:15 -0300

    database config url

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    WEBAPP_PORT : process.env.PORT || 3000
+    WEBAPP_PORT : process.env.PORT || 3000,
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/postgres'
 }
