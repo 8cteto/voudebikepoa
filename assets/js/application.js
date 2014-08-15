37597ac206768bbb383ab3ac38aa7e62387db233
Eduardo Bohrer <nbluisrs@gmail.com>
2014-08-15 03:07:42 -0300

    pegando a geolocalizacao do endereco destino

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,19 +1,25 @@
 $(function() {
     var targetFrom = $('#targetFrom'),
+          targetTo = $('#targetTo'),
+          currentPosition = undefined,
           geocoder = new google.maps.Geocoder();
-
 
     function initialize() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
-                findGeoLocation(targetFrom, position.coords.latitude, position.coords.longitude);
+                currentPosition = position.coords.latitude + ',' + position.coords.longitude;
+                findAddresByGeoLocation(targetFrom, position.coords.latitude, position.coords.longitude);
             });
         } else {
             setNotFoundFromTarget();
         }
     }
 
-    function findGeoLocation(targetElement, lat, lng) {
+    $('#routeButton').on('click', function() {
+        findGeoLocationByAddress(targetTo);
+    });
+
+    function findAddresByGeoLocation(targetElement, lat, lng) {
         var latlng = new google.maps.LatLng(lat, lng);
         
         geocoder.geocode({'latLng': latlng}, function(results, status) {
@@ -21,12 +27,19 @@ $(function() {
                 setNotFoundFromTarget();
                 return;
             }
+            targetFrom.val(results[1].formatted_address);
+        });
+    }
 
-            if (results[1]) {
-                targetFrom.val(results[1].formatted_address);
-            } else {
-                setNotFoundFromTarget();
-            }
+    function findGeoLocationByAddress(targetElement) {
+        var latlng = new google.maps.LatLng(currentPosition.split(','));
+        var address = targetElement.val() + ', Porto Alegre, RS, Brasil';
+
+        geocoder.geocode( { 'address': address, 'latLng': latlng} , function(results, status) {
+            if (status != google.maps.GeocoderStatus.OK) 
+                return;
+
+            targetElement.val(results[0].formatted_address);
         });
     }
 
