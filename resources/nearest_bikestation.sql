e801b840ab79e08aea3660b7da113db0b184fa1c
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2014-08-15 22:18:43 -0300

    Ajuste funcao nearest_bikestation para retornar uma tupla

--- resources/nearest_bikestation.sql
+++ resources/nearest_bikestation.sql
@@ -1,19 +1,14 @@
 CREATE EXTENSION IF NOT EXISTS cube;
 CREATE EXTENSION IF NOT EXISTS earthdistance;
 
-CREATE OR REPLACE FUNCTION nearest_bikestation(origin POINT)
-RETURNS POINT AS
+CREATE OR REPLACE FUNCTION nearest_bikestation(origin POINT, OUT name TEXT, OUT latitude FLOAT8, OUT longitude FLOAT8)
+AS
 $$
-	SELECT	point(latitude, longitude)
+	SELECT	nome,
+			latitude,
+			longitude
 	FROM	estacoes_bikepoa
 	ORDER	BY point(latitude, longitude) <-> origin
 	LIMIT	1;
-$$
-LANGUAGE sql;
-
-CREATE OR REPLACE FUNCTION nearest_bikestation_text(origin POINT)
-RETURNS TEXT AS
-$$
-	SELECT	translate(nearest_bikestation(origin)::TEXT, '()', '');
 $$
 LANGUAGE sql;
