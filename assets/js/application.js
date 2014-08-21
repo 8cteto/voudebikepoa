c6e7ab0e78c6566a5b290b82dfe58dae160fb7be
Eduardo Bohrer <nbluisrs@gmail.com>
2014-08-21 00:57:10 -0300

    refatorando e encapsulando objetos de mapController e router

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,14 +1,121 @@
 $(function() {
 
-	var 	targetFrom = $('#targetFrom'),
+	var MapController = function(mapContainer) {
+		var self = this
+
+		self.directionsService = new google.maps.DirectionsService();
+		self.directionsDisplay = new google.maps.DirectionsRenderer();
+		self.geocoder = new google.maps.Geocoder(),
+		self.bikeRacks = [];
+
+		this.initialize = function() {
+			var isDraggable = $(document).width() > 480;
+			var portoAlegre = new google.maps.LatLng(-30.0159,-51.1348);
+
+			var mapOptions = {
+				zoom:12,
+				center: portoAlegre,
+				scrollwheel:false,
+				draggable:isDraggable
+			};
+
+			self.map = new google.maps.Map(mapContainer, mapOptions);
+			self.directionsDisplay.setMap(self.map);
+		};
+
+		this.setCurrentPosition = function(lat, lng) {
+			self.createMarker('Meu Local', lat, lng);
+		};
+
+		this.addBikeRack = function(name, lat, lng) {
+			var marker = self.createMarker(name, lat, lng, '/images/icone-estacoes.gif');
+			self.bikeRacks.push(marker);
+		};
+
+		this.createMarker = function(name, lat, lng, icon) {
+			var position = new google.maps.LatLng(lat, lng);
+
+			var marker = new google.maps.Marker({
+				title: name,
+				position: position,
+				map: self.map,
+				icon: icon
+			});
+
+			google.maps.event.addListener(marker, 'click', function() {
+				if (self.currentInfoWindow) 
+					self.currentInfoWindow.close();
+
+				self.currentInfoWindow = new google.maps.InfoWindow({
+					content: name
+				});
+
+				self.currentInfoWindow.open(self.map, marker);
+			});
+
+			return marker;
+		}
+
+		this.setRoute = function(startPoint, endPoint) {
+			var request = {
+				origin: startPoint,
+				destination: endPoint,
+				travelMode: google.maps.TravelMode.WALKING
+			};
+
+			self.directionsService.route(request, function(response, status) {
+				if (status == google.maps.DirectionsStatus.OK) {
+					self.directionsDisplay.setDirections(response);
+				}
+			});
+		};
+
+		self.initialize();
+	};
+
+	var Geocoder = function() {
+		var 	self = this,
+			geocoder = new google.maps.Geocoder();
+
+		this.findAddresByGeoLocation = function(lat, lng, callback) {
+			var latlng = new google.maps.LatLng(lat, lng);
+			geocoder.geocode({'latLng': latlng}, function(results, status) {
+				if (status == google.maps.GeocoderStatus.OK) 
+					callback(results[0].formatted_address);
+			});
+		};
+
+		this.findGeoLocationByAddress = function(address, callback) {
+			var findAddress = address + ', Porto Alegre, RS, Brasil';
+
+			geocoder.geocode( { 'address': findAddress } , function(results, status) {
+				if (status == google.maps.GeocoderStatus.OK) {
+					var result = results[0];
+
+					callback({
+						formatted_address: result.formatted_address,
+						lat: result.geometry.location.lat(),
+						lng: result.geometry.location.lng()
+					});
+				}
+			});
+		};
+	};
+
+	var 	self = this,
+		targetFrom = $('#targetFrom'),
 		targetTo = $('#targetTo'),
 		inputForm = $('#map-input-form'),
-		directionsDisplay,
-		map,
-		directionsService = new google.maps.DirectionsService(),
-		geocoder = new google.maps.Geocoder(),
-		infowindow,
-		targetToClear =  $("#searchclear");
+		targetToClear =  $("#searchclear"),
+		mapContainer = $('#map-canvas');
+		mapController = undefined
+		geocoder = new Geocoder();
+
+	function initialize() {
+		mapController = new MapController(mapContainer[0]);
+		verifyCurrentLocation();
+		loadBikeRacks();
+	}
 
 	targetToClear.on('click', function(){
 		targetTo.val('');
@@ -18,35 +125,15 @@ $(function() {
 		$(this).attr('data-pos', '');
 	});
 
-	function initialize() {
-		initializeMap();
-		verifyCurrentLocation();
-		loadBikeRacks();
-	}
-
-	function initializeMap() {
-		var isDraggable = $(document).width() > 480 ? true : false;
-		var portoAlegre = new google.maps.LatLng(-30.0159,-51.1348);
-
-		var mapOptions = {
-			zoom:12,
-			center: portoAlegre,
-			scrollwheel:false,
-			draggable:isDraggable
-		};
-
-		map = new google.maps.Map($('#map-canvas')[0], mapOptions);
-
-		directionsDisplay = new google.maps.DirectionsRenderer();
-
-		directionsDisplay.setMap(map);
-	}
-
 	function verifyCurrentLocation() {
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(function(position) {
-				findAddresByGeoLocation(targetFrom, position.coords.latitude, position.coords.longitude);
-				markCurrentPositionOnMap(position);
+				mapController.setCurrentPosition(position.coords.latitude, position.coords.longitude);
+
+				geocoder.findAddresByGeoLocation(position.coords.latitude, position.coords.longitude, function(result) {
+					targetFrom.val(result).attr('data-pos', joinPosition(position.coords.latitude, position.coords.longitude));
+					inputForm.trigger('address-resolved');
+				});
 			});
 		} else {
 			setNotFoundFromTarget();
@@ -56,21 +143,8 @@ $(function() {
 	function loadBikeRacks() {
 		$.getJSON('/bikeRack/all', function(data) {
 			$.each(data, function(index, el) {
-				markPoint(el.latitude, el.longitude, el.nome);
+				mapController.addBikeRack(el.nome.replace(/\_/g, ' '), el.latitude, el.longitude);
 			});
-		});
-	}
-
-	function findAddresByGeoLocation(targetElement, lat, lng) {
-		var latlng = new google.maps.LatLng(lat, lng);
-		
-		geocoder.geocode({'latLng': latlng}, function(results, status) {
-			if (status != google.maps.GeocoderStatus.OK) {
-				setNotFoundFromTarget();
-				return;
-			}
-			targetFrom.val(results[0].formatted_address).attr('data-pos', joinPosition(lat, lng));
-			inputForm.trigger('address-resolved');
 		});
 	}
 
@@ -82,16 +156,10 @@ $(function() {
 		return position.split(',');
 	}
 
-	function findGeoLocationByAddress(targetElement) {
+	function verifyAddress(targetElement) {
 		var address = targetElement.val() + ', Porto Alegre, RS, Brasil';
-
-		geocoder.geocode( { 'address': address } , function(results, status) {
-			if (status != google.maps.GeocoderStatus.OK) 
-				return;
-
-			var result = results[0];
-
-			var position = joinPosition(result.geometry.location.lat(), result.geometry.location.lng());
+		geocoder.findGeoLocationByAddress(address, function(result) {
+			var position = joinPosition(result.lat, result.lng);
 			targetElement.val(result.formatted_address).attr('data-pos', position);
 			inputForm.trigger('address-resolved');
 		});
@@ -101,54 +169,8 @@ $(function() {
 		targetFrom.attr('placeholder', 'Digite o endereço de origem');
 	}
 
-	function markCurrentPositionOnMap(position){
-		var initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-		marcadorPraca = new google.maps.Marker({
-			position: initialLocation,
-			map: map,
-			title:"Meu Local",
-		});
-	}
-
-	function createRoute(startPoint, endPoint) {
-		var request = {
-			origin: startPoint,
-			destination: endPoint,
-			travelMode: google.maps.TravelMode.WALKING
-		};
-
-		directionsService.route(request, function(response, status) {
-			if (status == google.maps.DirectionsStatus.OK) {
-				directionsDisplay.setDirections(response);
-			}
-		});
-
-		focusMap();
-	}
-
-	function markPoint(latitude, longitude, markerName) {
-		var markerName = markerName.replace(new RegExp('_', 'g'),' ');
-		var position = new google.maps.LatLng(latitude, longitude);
-		var marker = new google.maps.Marker({
-			title: markerName,
-			position: position,
-			map: map,
-			icon:"/images/icone-estacoes.gif",
-		}); 
-
-	
-
-		google.maps.event.addListener(marker, 'click', function() {
-                if (infowindow) infowindow.close();
-                infowindow = new google.maps.InfoWindow({
-                    content: markerName
-                });
-	    		infowindow.open(map, marker);
-		});
-	}
-
 	function focusMap() {
-		$(window).scrollTop($('.map-container').offset().top);
+		$(window).scrollTop(map-container.offset().top);
 	}
 
 	function hasAddress(target) {
@@ -171,10 +193,10 @@ $(function() {
 			alert('Informe o local de destino!');
 
 		if (!addressAlreadyResolved(targetFrom))
-			findGeoLocationByAddress(targetFrom);
+			verifyAddress(targetFrom);
 
 		if (!addressAlreadyResolved(targetTo))
-			findGeoLocationByAddress(targetTo);
+			verifyAddress(targetTo);
 
 	}).on('address-resolved', function() {
 		if (!hasAddress(targetFrom) || !hasAddress(targetTo) || !addressAlreadyResolved(targetFrom) || !addressAlreadyResolved(targetTo))
@@ -186,7 +208,7 @@ $(function() {
 	function resolveNearestAddresses(startPosition, endPosition) {
 		$.get('/bikeRack/nearestBikeRack?startPosition=' + startPosition + '&endPosition=' + endPosition, function(data) {
 			//console.log('Building route', data);
-			createRoute(joinPosition(data.start.lat, data.start.lng), joinPosition(data.end.lat, data.end.lng))
+			mapController.setRoute(joinPosition(data.start.lat, data.start.lng), joinPosition(data.end.lat, data.end.lng))
 		});
 	}
 
