e91787680ed07e9bce10f75538bfe5d0f133542c
Eduardo Bohrer <nbluisrs@gmail.com>
2014-08-25 18:16:56 -0300

    tornando os links de repositorio dinamicos na pagina para facilitar a troca

--- helpers/configHelpers.js
+++ helpers/configHelpers.js
@@ -3,5 +3,13 @@ var packageJson = require('../package.json');
 module.exports = {
 	getAppVersion: function() {
 		return packageJson['version'];
+	},
+
+	getSourceCodeURL: function() {
+		return 'https://github.com/umovers/voudebike';
+	},
+
+	getContributorsURL: function() {
+		return 'https://github.com/umovers/voudebike/graphs/contributors';
 	}
 };
