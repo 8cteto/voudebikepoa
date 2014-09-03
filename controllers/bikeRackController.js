ed82e736a1ca7487bac55afbd4d27e21e6b0ae8b
Eduardo Bohrer <nbluisrs@gmail.com>
2014-09-03 00:06:35 -0300

    movendo responsabilidade de conhecer as colunas do banco para o model

--- controllers/bikeRackController.js
+++ controllers/bikeRackController.js
@@ -4,37 +4,10 @@ var 	url 		= require('url'),
 var BikeRackController = function() {
 	var self = this;
 
-	var OPERATION_STATUS = {
-		EM_OPERACAO : 'EO',
-		EM_MANUTENCAO: 'EM',
-		EM_IMPLANTACAO: 'EI'
-	};
-
-	this.translateOnlineStatus = function(onlineStatus, operationStatus) {
-		return onlineStatus == 'A' && OPERATION_STATUS.EM_OPERACAO == operationStatus;
-	};
-
-	this.translateAvailableSpots = function(totalSpots, occupiedSpots) {
-		return totalSpots - occupiedSpots;
-	}
-
-	this.createFromDataRow = function(row) {
-		return {
-			name: row['nome'].replace(/\_/g, ' '),
-			address: row['endereco'],
-			lat: row['latitude'],
-			lng: row['longitude'],
-			totalSpots: row['num_bicicletas'],
-			availableBikes: row['vagas_ocupadas'],
-			availableSpots: self.translateAvailableSpots(row['num_bicicletas'], row['vagas_ocupadas']),
-			online: self.translateOnlineStatus(row['status_online'], row['status_operacao'])
-		};
-	};
-
 	this.listAll = function(req, res) {
 		BikeRack.findAll(function(result) {
 			res.setHeader("Content-Type", "application/json");
-			res.end(JSON.stringify(result.rows.map(self.createFromDataRow)));
+			res.end(JSON.stringify(result));
 		});
 	};
 
--- model/bikeRack.js
+++ model/bikeRack.js
@@ -2,10 +2,40 @@ var BaseModel = require('./base');
 
 var BikeRack = function() {
 	var self = this;
+
 	BaseModel.apply(this);
 
+	var OPERATION_STATUS = {
+		EM_OPERACAO : 'EO',
+		EM_MANUTENCAO: 'EM',
+		EM_IMPLANTACAO: 'EI'
+	};
+
+	this.translateOnlineStatus = function(onlineStatus, operationStatus) {
+		return onlineStatus == 'A' && OPERATION_STATUS.EM_OPERACAO == operationStatus;
+	};
+
+	this.translateAvailableSpots = function(totalSpots, occupiedSpots) {
+		return totalSpots - occupiedSpots;
+	}
+
+	this.createFromDataRow = function(row) {
+		return {
+			name: row['nome'].replace(/\_/g, ' '),
+			address: row['endereco'],
+			lat: row['latitude'],
+			lng: row['longitude'],
+			totalSpots: row['num_bicicletas'],
+			availableBikes: row['vagas_ocupadas'],
+			availableSpots: self.translateAvailableSpots(row['num_bicicletas'], row['vagas_ocupadas']),
+			online: self.translateOnlineStatus(row['status_online'], row['status_operacao'])
+		};
+	};
+
 	this.findAll = function(callback) {
-		self.query('SELECT * FROM Estacoes_bikePOA', [], callback);
+		self.query('SELECT * FROM Estacoes_bikePOA', [], function(result) {
+			callback(result.rows.map(self.createFromDataRow));
+		});
 	};
 
 	this.getNearestStation = function(latlng, callback) {
