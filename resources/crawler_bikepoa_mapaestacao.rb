d62c7a1574a16b0d5b46284c509a5fb45b831fcf
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2015-11-03 16:41:36 -0200

    Ajustada URL do crawler para manter status das estações atualizadas (issue #48)

--- resources/crawler_bikepoa_mapaestacao.rb
+++ resources/crawler_bikepoa_mapaestacao.rb
@@ -2,7 +2,7 @@
 
 require 'net/http'
 
-content = Net::HTTP.get(URI('http://www.movesamba.com.br/bikepoa/mapaestacao.asp'))
+content = Net::HTTP.get(URI('http://ww2.mobilicidade.com.br/bikepoa/mapaestacao.asp'))
 content_encoded = content.force_encoding("ISO-8859-1").encode('utf-8')
 
 content_encoded.gsub(/\n|\r/, '').scan(/exibirEstacaMapa\(([\w\s\.\,\-\/\"\/\u00C0-\u02B0]+)\);/).each do |match| 
